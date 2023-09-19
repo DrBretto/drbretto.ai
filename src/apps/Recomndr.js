d9899df95b53fcce1f9b1e6c59593762cce81a93
DrBretto <59483898+DrBretto@users.noreply.github.com>
2023-09-19 11:04:35 -0400

    moved repository to MESH

--- src/apps/Recomndr.js
+++ src/apps/Recomndr.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
+//Alpha Vantage API Key = B91GX8DBBAR32QM0
+
 const Recomndr = () => {
   const [data, setData] = useState(null);
 
@@ -8,25 +10,24 @@ const Recomndr = () => {
     // Replace this URL with the URL of your Python API
     const url = 'http://localhost:5000/api/recomndr';
 
-    axios.get(url)
-      .then(response => {
+    axios
+      .get(url)
+      .then((response) => {
         setData(response.data);
       })
-      .catch(error => {
+      .catch((error) => {
         console.error('Error fetching data from API:', error);
       });
   }, []);
 
   if (!data) {
-    return (
-      <div>Loading...</div>
-    );
+    return <div>Loading...</div>;
   }
 
   return (
     <div>
       {/* Render your data here. This is just an example. */}
-      {data.map(item => (
+      {data.map((item) => (
         <div key={item.id}>
           <h2>{item.title}</h2>
           <p>{item.description}</p>
@@ -37,3 +38,10 @@ const Recomndr = () => {
 };
 
 export default Recomndr;
+
+
+
+
+
+
+//git remote set-url origin https://github.com/MESH-Interactive-Agency/drbretto.ai.git
