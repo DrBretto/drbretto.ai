96ee5647a3afa6cf2e795bb8db03918a6419ad35
DrBretto <59483898+DrBretto@users.noreply.github.com>
2023-09-19 09:16:18 -0400

    testing recomndr component

--- src/routes/HomePage.js
+++ src/routes/HomePage.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import LogoutButton from '../components/LogoutButton';
 import '../css/HomePage.css';
-import DraggableWidget from '../components/DraggableWidget';
+
 import Recomndr from '../apps/Recomndr';
-import MotherBrainOutput from '../components/MotherBrainOutput';
-import Chromostereopsis from '../apps/Chromostereopsis';
 
 const HomePage = () => {
   return (
     <div className="homepage-background d-flex justify-content-center align-items-center">
-      {/* <Chromostereopsis /> */}
+      <Recomndr />
       <LogoutButton />
     </div>
   );
