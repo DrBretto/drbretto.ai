6047d49455b2397c97831eeb9a3ffdefe57e7cfd
DrBretto <59483898+DrBretto@users.noreply.github.com>
2023-11-28 09:18:06 -0500

    updated stockChart

--- src/components/StockChart.js
+++ src/components/StockChart.js
@@ -1,20 +1,53 @@
-import React from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-
-// Fake stock data for 5 days
-const fakeStockData = [
-  { date: '2023-09-01', stockA: 200, stockB: 250 },
-  { date: '2023-09-02', stockA: 210, stockB: 240 },
-  { date: '2023-09-03', stockA: 205, stockB: 235 },
-  { date: '2023-09-04', stockA: 215, stockB: 255 },
-  { date: '2023-09-05', stockA: 220, stockB: 260 },
-];
+import React, { useState, useEffect } from 'react';
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from 'recharts';
 
 const StockChart = () => {
+  const [stockData, setStockData] = useState([]);
+
+  const mergeStockData = (stock1Data, stock2Data) => {
+    return stock1Data.map((item, index) => ({
+      date: item.date_time, // Assuming 'date_time' is the common time reference
+      JDST: item.normalized_price,
+      NUGT: stock2Data[index]?.normalized_price,
+    }));
+  };
+
+  useEffect(() => {
+    const fetchStockData = async () => {
+      try {
+        const response = await fetch(
+          'https://api-x0xg.onrender.com/api/stocks/last24hours'
+        );
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const { stock1Data, stock2Data } = await response.json();
+        const mergedData = mergeStockData(stock1Data, stock2Data);
+        setStockData(mergedData);
+      } catch (error) {
+        console.error('Error fetching stock data:', error);
+      }
+    };
+
+    fetchStockData();
+    const interval = setInterval(fetchStockData, 60000); // Fetch data every minute
+
+    return () => clearInterval(interval); // Clear interval on component unmount
+  }, []);
+
   return (
     <ResponsiveContainer width="100%" height={300}>
       <LineChart
-        data={fakeStockData}
+        data={stockData}
         margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
       >
         <CartesianGrid strokeDasharray="3 3" />
@@ -22,8 +55,8 @@ const StockChart = () => {
         <YAxis />
         <Tooltip />
         <Legend />
-        <Line type="monotone" dataKey="stockA" stroke="#8884d8" activeDot={{ r: 8 }} />
-        <Line type="monotone" dataKey="stockB" stroke="#82ca9d" />
+        <Line type="monotone" dataKey="JDST" stroke="#8884d8" activeDot={{ r: 8 }} />
+        <Line type="monotone" dataKey="NUGT" stroke="#82ca9d" />
       </LineChart>
     </ResponsiveContainer>
   );
