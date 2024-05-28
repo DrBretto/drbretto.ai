bf57351dca680e20c48095c780275b0b5c7df0b4
DrBretto <59483898+DrBretto@users.noreply.github.com>
2024-05-27 20:58:05 -0400

    removed interval

--- src/components/StockChart.js
+++ src/components/StockChart.js
@@ -17,7 +17,9 @@ const calculateSMA = (data, dataIndex, windowSize) => {
     if (i < windowSize - 1) {
       smaValues.push(null); // Initial values are null until window size is reached
     } else {
-      const sum = data.slice(i - windowSize + 1, i + 1).reduce((acc, val) => acc + val[dataIndex], 0);
+      const sum = data
+        .slice(i - windowSize + 1, i + 1)
+        .reduce((acc, val) => acc + val[dataIndex], 0);
       const average = sum / windowSize;
       smaValues.push(average);
     }
@@ -27,7 +29,9 @@ const calculateSMA = (data, dataIndex, windowSize) => {
 
 const fetchLatestPrediction = async () => {
   try {
-    const response = await fetch(`https://api-x0xg.onrender.com/api/data/latest-prediction`);
+    const response = await fetch(
+      `https://api-x0xg.onrender.com/api/data/latest-prediction`
+    );
     if (!response.ok) {
       throw new Error(`Network response was not ok`);
     }
@@ -37,11 +41,16 @@ const fetchLatestPrediction = async () => {
     const maxNUGT = Math.max(...predictionData.stock2_prediction);
     const minJDST = Math.min(...predictionData.stock1_prediction);
     const minNUGT = Math.min(...predictionData.stock2_prediction);
-    const normalizedData = predictionData.stock1_prediction.map((value, index) => ({
-      date: `Prediction ${index + 1}`,
-      JDST: ((value - minJDST) / (maxJDST - minJDST)) * 100, // Normalize JDST data
-      NUGT: ((predictionData.stock2_prediction[index] - minNUGT) / (maxNUGT - minNUGT)) * 100, // Normalize NUGT data
-    }));
+    const normalizedData = predictionData.stock1_prediction.map(
+      (value, index) => ({
+        date: `Prediction ${index + 1}`,
+        JDST: ((value - minJDST) / (maxJDST - minJDST)) * 100, // Normalize JDST data
+        NUGT:
+          ((predictionData.stock2_prediction[index] - minNUGT) /
+            (maxNUGT - minNUGT)) *
+          100, // Normalize NUGT data
+      })
+    );
     return normalizedData;
   } catch (error) {
     console.error(`Error fetching latest prediction:`, error);
@@ -64,9 +73,9 @@ const StockChart = () => {
     };
 
     fetchPredictionData();
-    const interval = setInterval(fetchPredictionData, 60000); // Update every minute
+    // const interval = setInterval(fetchPredictionData, 60000); // Update every minute
 
-    return () => clearInterval(interval);
+    // return () => clearInterval(interval);
   }, []);
   // Calculate SMA for JDST and NUGT with a window size of 5 (adjust as needed)
   const smaWindowSize = 5;
@@ -87,18 +96,43 @@ const StockChart = () => {
         margin={{ top: 20, right: 20, left: 20, bottom: 20 }}
       >
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="date" hide={true}/>
+        <XAxis dataKey="date" hide={true} />
         <YAxis domain={[0, 100]} hide={true} />
         <Tooltip />
         <Legend />
-        <Line type="monotone" dataKey="JDST" stroke="#8884d8" strokeWidth={1} strokeOpacity={0.25} dot={false} />
-        <Line type="monotone" dataKey="NUGT" stroke="#82ca9d" strokeWidth={1} strokeOpacity={0.25} dot={false} />
-        <Line type="monotone" dataKey="JDST_SMA" stroke="blue" strokeWidth={3} dot={false} />
-        <Line type="monotone" dataKey="NUGT_SMA" stroke="green" strokeWidth={3} dot={false} />
+        <Line
+          type="monotone"
+          dataKey="JDST"
+          stroke="#8884d8"
+          strokeWidth={1}
+          strokeOpacity={0.25}
+          dot={false}
+        />
+        <Line
+          type="monotone"
+          dataKey="NUGT"
+          stroke="#82ca9d"
+          strokeWidth={1}
+          strokeOpacity={0.25}
+          dot={false}
+        />
+        <Line
+          type="monotone"
+          dataKey="JDST_SMA"
+          stroke="blue"
+          strokeWidth={3}
+          dot={false}
+        />
+        <Line
+          type="monotone"
+          dataKey="NUGT_SMA"
+          stroke="green"
+          strokeWidth={3}
+          dot={false}
+        />
       </LineChart>
     </ResponsiveContainer>
   );
 };
-
 
 export default StockChart;
